dfaa556601fe52f2ab87a29cce52f51386d835e1
Louise Whittle <louisewhittle@Louises-MacBook-Pro.local>
2018-03-02 14:33:35 +0000

    add edit and delete tests and actions

--- test/integration/recipes_edit_test.rb
+++ test/integration/recipes_edit_test.rb
@@ -21,9 +21,17 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_select 'div.panel-body'
   end
 
-  test "sucessfully edit a recipe" do
-
-
-  end
+  test "successfully edit a recipe" do
+   get edit_recipe_path(@recipe)
+   assert_template 'recipes/edit'
+   updated_name = "updated recipe name"
+   updated_description = "updated recipe description"
+   patch recipe_path(@recipe), params: { recipe: { name: updated_name, description: updated_description } }
+   assert_redirected_to @recipe
+   assert_not flash.empty?
+   @recipe.reload
+   assert_match updated_name, @recipe.name
+   assert_match updated_description, @recipe.description
+ end
 
 end
