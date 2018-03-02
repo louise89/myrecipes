68f31508b89000fde6e48ec680b4eacb221c36c4
Louise Whittle <louisewhittle@Louises-MacBook-Pro.local>
2018-03-02 17:19:55 +0000

    refactor and clean layout

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,11 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update]
 
 def index
   @recipes = Recipe.all
 end
 
 def show
-  @recipe = Recipe.find(params[:id])
 end
 
 def new
@@ -24,7 +24,6 @@ def create
 end
 
 def edit
-  @recipe = Recipe.find(params[:id])
   if @recipe.save
   else
     render 'edit'
@@ -32,7 +31,6 @@ def edit
 end
 
 def update
-  @recipe = Recipe.find(params[:id])
   if @recipe.update(recipe_params)
     flash[:success] = "Recipe was updated successfully!"
     redirect_to recipe_path(@recipe)
@@ -49,6 +47,10 @@ end
 
 
 private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 
     def recipe_params
       params.require(:recipe).permit(:name, :description)
