1996103287b34fa7d2d334f4dbd1bd2421bf4a65
Louise Simpson <louise.simpson@onthebeach.co.uk>
2018-07-13 17:19:10 +0100

    fix styling, and start amending style guide

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,7 +14,7 @@ end
 
 def create
   @recipe = Recipe.new(recipe_params)
-  @recipe.chef = Chef.first
+  @recipe.chef = Chef.current_chef
   if @recipe.save
     flash[:success] = "Recipe was created successfully!"
     redirect_to recipe_path(@recipe)
